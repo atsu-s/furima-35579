fb72161f437d9b7670fa79202c57c1797b7c8da9
atsu-s <soneatsu49@gmail.com>
2021-04-28 14:23:17 +0900

    ユーザー管理機能実装

--- db/migrate/20210427074426_create_items.rb
+++ db/migrate/20210427074426_create_items.rb
@@ -2,8 +2,8 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
-      t.string :title, null: false
-      t.text :text, null: false
+      t.string  :title, null: false
+      t.text    :text, null: false
       t.integer :category_id, null: false
       t.integer :status_id, null:false
       t.integer :delivery_charge_id, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_074426) do
+ActiveRecord::Schema.define(version: 2021_04_27_101527) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -45,5 +45,4 @@ ActiveRecord::Schema.define(version: 2021_04_27_074426) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "items", "users"
 end
