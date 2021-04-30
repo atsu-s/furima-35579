939582df8e9acbb20fbb4a26bfb607c5280dbda2
atsu-s <soneatsu49@gmail.com>
2021-04-30 12:05:43 +0900

    各idバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,10 @@ extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :delivery_charge
   belongs_to :prefecture
   belongs_to :period
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 }
+  validates :delivery_charge_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :period_id, numericality: { other_than: 1 }
 end
