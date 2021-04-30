ab85dcd63f58f1c5e73aef4e5f0fc037c3e7d597
atsu-s <soneatsu49@gmail.com>
2021-04-30 16:41:46 +0900

    バリデーション追記・エラー文表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,23 @@
 class Item < ApplicationRecord
-belongs_to :user
-has_one_attached :image
 
-extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :status
-  belongs_to :delivery_charge
-  belongs_to :prefecture
-  belongs_to :period
+  validates :title, { presence: true, length: {maximum: 40}}
+  validates :text, presence: true
+  validates :price, { presence: true, numericality: { in: 300..9999999 }}
+  validates :image, presence: true
 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :status_id, numericality: { other_than: 1 }
-  validates :delivery_charge_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :period_id, numericality: { other_than: 1 }
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :delivery_charge
+    belongs_to :prefecture
+    belongs_to :period
+
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :status_id, numericality: { other_than: 1 }
+    validates :delivery_charge_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :period_id, numericality: { other_than: 1 }
 end
