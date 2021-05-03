fe2aa35980086f6c03a03f55c0741ce36c0b93fa
atsu-s <soneatsu49@gmail.com>
2021-05-03 16:08:57 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_login, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -23,13 +24,10 @@ class ItemsController < ApplicationController
   end
   
   def edit
-    unless user_signed_in? && current_user.id == @item.user.id
-      redirect_to action: :index
-    end
+    
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -44,5 +42,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def set_login
+    unless current_user.id == @item.user.id
+      redirect_to action: :index
+    end
   end
 end
