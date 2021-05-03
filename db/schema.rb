0eb64c180f5616c9645b2ed530364b45e33d2402
atsu-s <soneatsu49@gmail.com>
2021-05-03 18:04:50 +0900

    buyersテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(version: 2021_05_03_085612) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "buyers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city_name", null: false
+    t.string "address", null: false
+    t.string "building_name"
+    t.string "tell_number", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_buyers_on_order_id"
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "text", null: false
@@ -76,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_05_03_085612) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "buyers", "orders"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
